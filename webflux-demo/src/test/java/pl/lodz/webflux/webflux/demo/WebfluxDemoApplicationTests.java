8786abd834b7916e04ac8679c0cdc52915b910c9
jakub flaszka <244008@edu.p.lodz.pl>
2022-11-16 20:52:54 +0100

    basic project

---
+++ webflux-demo/src/test/java/pl/lodz/webflux/webflux/demo/WebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.webflux.webflux.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
